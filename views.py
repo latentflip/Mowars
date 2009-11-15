294f2b5514df667a3923f7cd980d29437e0c2062
latentflip <latentflip@gmail.com>
2009-11-15 13:08:33 +0000

    added views.py

---
+++ views.py
@@ -0,0 +1,9 @@
+import cgi, os
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+from google.appengine.api import images
+
